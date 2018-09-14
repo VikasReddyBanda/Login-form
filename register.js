643a8f94100f2ea407db5935d9daff2af47584aa
Vikas Reddy <VikasReddyBanda@users.noreply.github.com>
2018-09-14 17:36:30 +0530

    form edit and Update

--- register.js
+++ register.js
@@ -1,91 +1,187 @@
 var express = require("express");
-var app     = express();
-var path    = require("path");
-var mysql = require('mysql');
-var session= require('express-session');
-var bodyParser = require('body-parser');
-var user=require('./routes/user')
-var ejs=require('ejs');
-var fs = require('fs');
+var app = express();
+var path = require("path");
+var mysql = require("mysql");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var user = require("./routes/user");
+var ejs = require("ejs");
+var fs = require("fs");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 }
-}));
-app.set('view engine','html');
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+  })
+);
+
+app.set("view engine", "ejs");
+
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
   database: "mydb"
 });
+
 con.connect();
-global.db=con;
-app.get('/edit',function(req,res){
- if(req.session.user){
-  res.sendFile(path.join(__dirname+'/createaccount.html'));
- }else{
+global.db = con;
+
+app.get("/edit/:id", function (req, res) {
+  var id = req.params.id;
+  //var sess=req.sess.user;
+  // if (sess) {
+  // var name=req.session.name;
+  //var email=req.session.email;
+  var sql = "select * from employee where id='" + id + "'";
+  db.query(sql, function (err, result) {
+    if (result.length) {
+      var message = " ";
+      res.render("edituser.ejs", { message: message, data: result });
+      //res.sendFile(path.join(__dirname + "/createaccount.html"));
+    } else {
       res.redirect("/");
-  }
-   });
+    }
+  });
 
-app.get('/',function(req,res){
-  var message=" ";
-  res.render('login1.ejs',{message:message});
+  // } else {
+  // res.redirect("/");
+  // }
 });
-app.post('/login',user.login);//function(req,res,next){
-//	if(req.session.user=null){
-	//	res.redirect('/');
-	//}else{
-	
-/*var nam=req.body.name;
-var pass=req.body.password;
-con.connect(function(err){
-  if(err) throw err;
-  var sql="select * from employee where username='"+nam+"' && password='"+pass+"'";
-  con.query(sql,function (err,result) {
-    if(result.length){
-      req.session.user=result[0];
-      res.render('content.ejs', {data:result}); 
-    }else
-    res.render('/');
- });
+
+app.post("/updateuser/:id", function (req, res) {
+  //var name=req.body.ename;
+  var id = req.params.id;
+  var name=req.body.ename;
+  var email = req.body.eemail;
+  var username = req.body.eusername;
+  var password = req.body.epassword;
+  var sql = "update employee set name='" + name + "', email='" + email + "',username='" + username + "',password='" + password + "' where id='" + id + "'";
+  db.query(sql, function (err, result) {
+    if (result) {
+      var sql="select *from employee where id='"+req.params.id+"'";
+      db.query(sql, function (err, result) {
+      var message = "Sucessfully updated your details!"
+      res.render("content.ejs", { data: result, message: message })
+      });
+    } else {
+      var message = "please enter valid credentials!"
+      res.render("edituser.ejs", { data: result, message: message })
+    }
+  });
+
 });
-	};
-});*/
-app.get('/logout',user.logout);/*function(req,res){
-  
- req.session.destroy(function(err){
-   res.redirect("/");
-  });
-});*/
-app.post('/createaccount',user.createaccount);/*function(req,res){
-  res.sendFile(path.join(__dirname+'/createaccount.html'));
-});*/
-/*app.post('/csubmit',function(req,res){
-var sq="select *from employee where email='"+req.body.email+"'";
-  var Name=req.body.name;
-  var Email=req.body.email;
-  var Username=req.body.username;
-  var Password=req.body.password;
- res.send('Account Created Successfully');
-  con.connect(function(err) {
-   if (err) throw err;
-   var sql = "INSERT INTO employee (name, email,username,password) VALUES ('"+Name+"', '"+Email+"','"+Username+"','"+Password+"')";
-   con.query(sql, function (err,result) {
-    if (err) throw err;
-    res.send('success');
-    console.log(result[0]);  
-  });
+
+app.get("/", function (req, res) {
+  var s = "user" in req.session;
+  if (s) {
+    var results = req.session.result;
+    var sess = req.session.user;
+    res.end;
+    console.log("========>", req.session);
+    var sql =
+      "select * from employee where username='" +
+      req.session.user +
+      "' && password='" +
+      req.session.password +
+      "'";
+    db.query(sql, function (err, results) {
+      if (results.length) {
+        var sess = req.session.user;
+        res.locals.res = res;
+        res.setHeader("Pragma", "no-cache");
+        res.setHeader("Expires", "0");
+        res.setHeader(
+          "Cache-Control",
+          "no-store, no-cache, max-age=0, must-revalidate, private"
+        );
+        res.render("content.ejs", { data: results });
+      }
     });
+  } else {
+    var message = " ";
+    // res.redirect("/login2");
+    res.setHeader(
+      "Cache-Control",
+      "no-store, no-cache, max-age=0, must-revalidate, private"
+    );
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+    res.render("login1.ejs", { sess: sess, message: message });
+  }
+});
 
-      res.end();
-}); 
-*/
+// app.get("/login2", (req, res) => {
+//   var s = "user" in req.session;
+//   if (s) {
+//     var results = req.session.result;
+//     var sess = req.session.user;
+//     // res.render("content.ejs", { sess: sess, data: results });
+//     res.redirect("/login");
+//   } else {
+//     var message = " ";
+//     // res.redirect("/login2");
+//     res.setHeader(
+//       "Cache-Control",
+//       "no-store, no-cache, max-age=0, must-revalidate, private"
+//     );
+//     res.setHeader("Pragma", "no-cache");
+//     res.setHeader("Expires", "0");
+//     res.render("login1.ejs", { message: message });
+//   }
+// });
 
+app.get("/login", (req, res) => {
+  var s = "user" in req.session;
+  if (s) {
+    var sql =
+      "select * from employee where username='" +
+      req.session.user +
+      "' && password='" +
+      req.session.password +
+      "'";
+    db.query(sql, function (err, results) {
+      if (results.length) {
 
-app.listen(3000);
-console.log("Running at Port 3000");
+        res.locals.res = res;
+        res.setHeader("Pragma", "no-cache");
+        res.setHeader("Expires", "0");
+        res.setHeader(
+          "Cache-Control",
+          "no-store, no-cache, max-age=0, must-revalidate, private"
+        );
+        var message = " ";
+        res.render("content.ejs", { message: message, data: results });
+      } else {
+        var message = "failed";
+        res.render("login1.ejs", { message: message });
+      }
+    });
+  } else {
+    var message = "please login again";
+    res.setHeader("Pragma", "no-cache");
+    res.setHeader("Expires", "0");
+    res.setHeader(
+      "Cache-Control",
+      "no-store, no-cache, max-age=0, must-revalidate, private"
+    );
+    res.render("login1.ejs", { message: message });
+  }
+});
+
+app.post("/login", user.login);
+app.get("/logout", user.logout);
+app.post("/csubmit", user.createaccount);
+//app.get("/csubmit", user.createaccount);
+ app.get("/fasak",function(req,res){
+ 
+   res.sendFile(path.join(__dirname + "/createaccount.html"));
+ });
+
+app.listen(5000);
+console.log("Running at Port 5000");
--- routes/user.js
+++ routes/user.js
@@ -1,46 +1,76 @@
-exports.login=function(req,res){
-    var message = '';
-    if(req.method=="POST"){
-        var nam=req.body.name;
-        var pass=req.body.password;
-        var sql="select * from employee where username='"+nam+"' && password='"+pass+"'";
-        db.query(sql,function(err,results){
-            if(results.length){
-                req.session.user=results[0];
-                var sess=req.session.user;
-                res.locals.res = res;
-                res.render('content.ejs',{sess:sess,data:results});
-            }else{
-                message = 'Wrong Credentials.';
-                res.render('login1.ejs', { message: message });
-            }
-        });
-    }else{
-        res.render('index.ejs', { message: message });
+exports.login = function (req, res) {
+  var s = "user" in req.session;
+  console.log(req.session);
+  if (s) {
+    res.render("login1.ejs", { message: "session expired" });
+  } else {
+    var message = "";
+    if (req.method == "POST") {
+      var nam = req.body.name;
+      var pass = req.body.password;
+      var sql =
+        "select * from employee where username='" +
+        nam +
+        "' && password='" +
+        pass +
+        "'";
+      db.query(sql, function (err, results) {
+        if (results.length) {
+          req.session.result = results;
+          req.session.user = nam;
+          req.session.password = pass;
+          var sess = req.session.user;
+          res.locals.res = res;
+          // res.redirect("/login");
+          // res.setHeader("Pragma", "no-cache");
+          // res.setHeader("Expires", "0");
+          // res.setHeader(
+          //   "Cache-Control",
+          //   "no-store, no-cache, max-age=0, must-revalidate, private"
+          // );
+          res.redirect("/login");
+          // res.render("content.ejs", { sess: sess, data: results });
+        } else {
+          res.render("login1.ejs", { message: message });
+        }
+      });
     }
+  }
 };
+
 /*----------------------------------Logout-----------------------------------------------*/
-exports.logout=function(req,res){
-   
-    req.session.destroy(function(err){
-        //res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
-        res.cookie();
-        res.redirect("/");
-    });
+
+exports.logout = function (req, res) {
+  req.session.destroy(function (err) {
+    //res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+    res.cookie();
+    res.redirect("/");
+  });
+  console.log(req.session);
 };
+
 /*----------------------------------createaccount----------------------------------------*/
-exports.createaccount=function(req,res){
-    if(req.method="POST"){
-        var Name=req.body.name;
-  var Email=req.body.email;
-  var Username=req.body.username;
-  var Password=req.body.password;
-  var sql = "INSERT INTO employee (name, email,username,password) VALUES ('"+Name+"', '"+Email+"','"+Username+"','"+Password+"')";
- db.query(sql,function(err,result){
-    message = "Succesfully! Your account has been created.";
-    res.render('login1.ejs', { message: message });
- });   
- }else{
-    res.sendFile(path.join(__dirname+'/createaccount.html'));
- }
-};  
+exports.createaccount = function (req, res) {
+  if ((req.method = "POST")) {
+    var Name = req.body.name;
+    var Email = req.body.email;
+    var Username = req.body.username;
+    var Password = req.body.password;
+    var sql =
+      "INSERT INTO employee (name, email,username,password) VALUES ('" +
+      Name +
+      "', '" +
+      Email +
+      "','" +
+      Username +
+      "','" +
+      Password +
+      "')";
+    db.query(sql, function (err, result) {
+      message = "Succesfully! Your account has been created.";
+      res.render("login1.ejs", { message: message });
+    });
+  } else {
+    res.sendFile(path.join(__dirname + "/createaccount.html"));
+  }
+};
